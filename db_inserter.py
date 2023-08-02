5440d7ae1411d9c15f4b32668baea9bbc482aca5
@HawkEyes-OSINT <h4wkd3v@gmail.com>
2023-08-02 14:59:46 +0300

    added try, except on csv line

--- db_inserter.py
+++ db_inserter.py
@@ -36,50 +36,53 @@ def insert_data(db_path, csv_path):
         reader = csv.reader(file, delimiter=detect_csv_delimiter(csv_path))
         next(reader) # skip first line
         for line in reader:
-            for instuction in instructions:
-                # get istructions
-                table, columns, strip_char = instuction
+            try:
+                for instuction in instructions:
+                    # get istructions
+                    table, columns, strip_char = instuction
 
-                # get data from csv
-                values = [line[col[0]:col[len(col)-1]+1] for col in columns]
-                if table == 'names' and not values:
-                    # names is a required table
-                    print('Must provide a name value')
-                    return
+                    # get data from csv
+                    values = [line[col[0]:col[len(col)-1]+1] for col in columns]
+                    if table == 'names' and not values:
+                        # names is a required table
+                        print('Must provide a name value')
+                        return
 
-                # format values
-                formated_values = []
-                tmp_values = []
-                if strip_char != '':
+                    # format values
+                    formated_values = []
+                    tmp_values = []
+                    if strip_char != '':
+                        for value in values:
+                            # create sublist
+                            merged = ''
+                            for v in value:
+                                merged += v
+                            value = merged
+                            for char in strip_char:
+                                value = value.replace(char, '')
+                            words = value.split()
+                            [tmp_values.append([word]) for word in words if word]
+                        values = tmp_values
                     for value in values:
-                        # create sublist
-                        merged = ''
-                        for v in value:
-                            merged += v
-                        value = merged
-                        for char in strip_char:
-                            value = value.replace(char, '')
-                        words = value.split()
-                        [tmp_values.append([word]) for word in words if word]
-                    values = tmp_values
-                for value in values:
-                    if table == 'names' or table == 'addresses':
-                        formated_values.append(format_title(value))
-                    elif table == 'emails':
-                        formated_values.append(format_lower(value[0]))
-                    elif table == 'phones':
-                        formated_values.append(format_phone(value[0]))
+                        if table == 'names' or table == 'addresses':
+                            formated_values.append(format_title(value))
+                        elif table == 'emails':
+                            formated_values.append(format_lower(value[0]))
+                        elif table == 'phones':
+                            formated_values.append(format_phone(value[0]))
+                        else:
+                            formated_values.append(value[0])
+
+                    # insert data to DB
+                    if table == 'names':
+                        cur.execute(qname, (formated_values[0],))
+                        name_id = cur.lastrowid
                     else:
-                        formated_values.append(value[0])
+                        [cur.execute(qtable.format(table), (name_id, value, source)) for value in formated_values if value]
+            except Exception as e:
+                print('[!]', e)
 
-                # insert data to DB
-                if table == 'names':
-                    cur.execute(qname, (formated_values[0],))
-                    name_id = cur.lastrowid
-                else:
-                    [cur.execute(qtable.format(table), (name_id, value, source)) for value in formated_values if value]
-
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
     print('[+] Data inserted to DB')
     
